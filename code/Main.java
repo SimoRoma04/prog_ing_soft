5dcfa7fe4218cec48463ae4159eef3a078996202
Federico Aguzzi <federico.aguzzi@icloud.com>
2022-01-24 14:00:23 +0100

    Readapted for the new classes

--- code/Main.java
+++ code/Main.java
@@ -5,13 +5,46 @@ import java.awt.Dimension;
 import java.util.ArrayList;
 
 import javax.swing.JPanel;
+
+import controller.Controller;
+import model.Course;
+import model.Option;
+import model.Quiz;
+
 import javax.swing.JFrame;
 
 public class Main{
 
 	public static void main(String[] args) {
-		//new AnswerQuestion(new TestController("Piero"));
-		new Home(new TestController("pierino"));
+		
+		
+		Course ingegneriaPieristica = new Course("Ingegneria del Piero");
+		
+		Option o = new Option();
+		o.setText("Risposta 1");
+		o.setIsRight(true);
+		
+		ArrayList<Option> options = new ArrayList<Option>();
+		options.add(o);
+		
+		Quiz q = new Quiz();
+		q.setName("Domanda di prova");
+		q.setQuestion("Qual è la domanda?");
+		q.setOptions(options);
+	
+		Quiz r = new Quiz();
+		r.setName("Domanda di prova");
+		r.setQuestion("Qual è la domanda?");
+		r.setOptions(options);
+		
+		
+		ingegneriaPieristica.addQuiz(q);
+		ingegneriaPieristica.addQuiz(r);
+		
+		Controller piero = new Controller(ingegneriaPieristica.getName(), ingegneriaPieristica);
+		
+		new AnswerQuestion(piero);
+		// new Home(new TestController("pierino"));
 		System.out.println("Sto eseguedo il main");
 	}
 }
