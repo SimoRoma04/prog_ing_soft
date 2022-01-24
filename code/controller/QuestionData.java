fc88ba237c9fd1b83a1d0ff10d66cc7a3bd20e9f
Federico Aguzzi <federico.aguzzi@icloud.com>
2022-01-24 13:59:13 +0100

    Changed data type of m_color

--- code/controller/QuestionData.java
+++ code/controller/QuestionData.java
@@ -1,19 +1,25 @@
 package controller;
 
+import java.awt.Color;
+
 import model.Option;
 
 public class QuestionData {
 	
 	private Option m_option;
-	String m_color;
+	Color m_color;
 	
-	public QuestionData(Option o, String c) {
+	public QuestionData(Option o, Color c) {
 		m_option = o;
 		m_color = c;
 	}
 	
 	public Option getOption() {
 		return m_option;
+	}
+	
+	public Color getColor() {
+		return m_color;
 	}
 
 }
