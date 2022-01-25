0072802ccbdb836b11b3510d0d44d3d5538e74ad
cripza <43786707+cripza@users.noreply.github.com>
2022-01-25 18:37:16 +0100

    CsvParser: refactored csvParser.

--- code/parser/CsvParser.java
+++ code/parser/CsvParser.java
@@ -5,6 +5,8 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.NoSuchElementException;
 import java.util.StringTokenizer;
 
 import model.Option;
@@ -16,53 +18,73 @@ public class CsvParser {
 		
 		ArrayList<Question> questions = new ArrayList<Question>();
 		
+		File f = new File(path);
+		
 		try {
 			FileReader fr = new FileReader(f);
 			BufferedReader br = new BufferedReader(fr);
 			
 			String line;
+			int rowNumber = 0;
 			while((line = br.readLine()) != null) {
+				rowNumber++;
 				String chapter, question, image;
 				Boolean hasImage;
 				ArrayList<Option> options = new ArrayList<Option>();
+				String[] row = line.split("\t");
 				
-				StringTokenizer st = new StringTokenizer(line, ";");
+				if(row.length < 5) {
+					System.out.println("Unexpected content found on line " + rowNumber + " of input file");
+					continue;
+				}
 				
 				// Capitolo
-				String current = st.nextToken();
-				if(current.equals(""))
-					chapter = "Capitolo non specificato";
-				else
-					chapter = current;
+				String current = row[0];
+				if(current.equals("")) continue;
+				chapter = current;
 				
 				// Testo della domanda
-				current = st.nextToken();
-				if(current.equals(""))
-					question = "Domanda mancante";
-				else
-					question = current;
+				current = row[1];
+				if(current.equals("")) continue;
+				question = current;
+				
 				
 				// Path dell'immagine (se c'è);
-				current = st.nextToken();
+				current = row[2];
+				hasImage = true;
 				if(current.equals("")) {
 					hasImage = false;
 					image = "";
 				}
-				else {
+				else 
+				{
 					hasImage = true;
 					image = current;
 				}
 				
+				
 				// Opzione corretta
-				current = st.nextToken();
+				current = row[3];
+				if(current.equals("")) continue;
 				options.add(new Option(current, true));
 				
-				// Aggiunta delle altre risposte
-				while(st.hasMoreTokens()) {
-					current = st.nextToken();
-					if(current.equals(""))
-						break;
-					options.add(new Option(current, false));
+				
+				// Opzione sbagliata 1 (obbligatoria)
+				current = row[4];
+				if(current.equals("")) continue;
+				options.add(new Option(current, false));
+				
+				
+				// Opzione sbagliata 2 (facoltativa)
+				if(row.length > 5) {
+					current = row[5];
+					if(!current.equals("")) options.add(new Option(current, false));
+				}
+				
+				// Opzione sbagliata 3 (facoltativa)
+				if(row.length > 6) {
+					current = row[6];
+					if(!current.equals("")) options.add(new Option(current, false));
 				}
 				
 				questions.add(new Question(chapter, question, hasImage, image, options));
