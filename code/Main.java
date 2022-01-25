4485d80b8d5c26de12fedfad27c7655918d923c1
cripza <43786707+cripza@users.noreply.github.com>
2022-01-25 18:45:35 +0100

    Main: fixed imports.

--- code/Main.java
+++ code/Main.java
@@ -1,12 +1,5 @@
-
-
-import java.awt.BorderLayout;
-import java.awt.Color;
-import java.awt.Dimension;
-import java.util.ArrayList;
-
-import javax.swing.JPanel;
-import javax.swing.JFrame;
+import view.Home;
+import view.TestController;
 
 public class Main{
 
