bf00e85e652e68a3331564032f111b0048c31bb1
Federico Aguzzi <federico.aguzzi@icloud.com>
2022-01-25 13:50:09 +0100

    Separated Main from the View module

---
+++ code/Main.java
@@ -0,0 +1,19 @@
+
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.util.ArrayList;
+
+import javax.swing.JPanel;
+import javax.swing.JFrame;
+
+public class Main{
+
+	public static void main(String[] args) {
+		//new AnswerQuestion(new TestController("Piero"));
+		new Home(new TestController("pierino"));
+		//new AnswerQuestion(new TestController("ofnoefn"));
+		System.out.println("Sto eseguedo il main");
+	}
+}
