5b451b8b80b4085f239cad3ea7a5ae22f58faeae
cripza <43786707+cripza@users.noreply.github.com>
2022-01-29 19:07:40 +0100

    Main: changes needed due to debug needs

--- code/Main.java
+++ code/Main.java
@@ -1,12 +1,15 @@
+import model.GuiManager;
 import view.Home;
 import view.TestController;
 
 public class Main{
+	private static GuiManager m_guiManager;
 
 	public static void main(String[] args) {
 		//new AnswerQuestion(new TestController("Piero"));
-		new Home(new TestController("pierino"));
+		//new Home(new TestController("pierino"));
 		//new AnswerQuestion(new TestController("ofnoefn"));
+		m_guiManager = new GuiManager();
 		System.out.println("Sto eseguedo il main");
 	}
 }
