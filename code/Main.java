06eb0161aed9ee407d3158934e8f6be5df90591b
cripza <43786707+cripza@users.noreply.github.com>
2022-01-29 22:52:30 +0100

    Main: refactor due to debug needs

--- code/Main.java
+++ code/Main.java
@@ -1,15 +1,22 @@
+import java.util.ArrayList;
+
 import model.GuiManager;
-import view.Home;
-import view.TestController;
+import model.QuizManager;
 
 public class Main{
 	private static GuiManager m_guiManager;
+	private static QuizManager m_quizManager;
 
 	public static void main(String[] args) {
 		//new AnswerQuestion(new TestController("Piero"));
 		//new Home(new TestController("pierino"));
 		//new AnswerQuestion(new TestController("ofnoefn"));
 		m_guiManager = new GuiManager();
+		m_quizManager = new QuizManager();
+		
+		m_quizManager.loadFile("./docs/domande.tsv");
+		ArrayList<String> chapterTitles = m_quizManager.getChapterNameList();
+		
 		System.out.println("Sto eseguedo il main");
 	}
 }
