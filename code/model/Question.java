af7601ff1bb62ecca06c11f0787b0ee0e0687a5c
cripza <43786707+cripza@users.noreply.github.com>
2022-01-29 22:49:37 +0100

    Question: removed misplaced data
    This was done to prevent data duplication

--- code/model/Question.java
+++ code/model/Question.java
@@ -4,15 +4,12 @@ import java.util.ArrayList;
 import javax.swing.ImageIcon;
 
 public class Question {
-	
-	private String m_chapter = null;
 	private String m_text = null;
 	private boolean m_hasImage = false;
 	private Image m_image = null;
 	private ArrayList<Option> m_options = null;
 	
-	public Question(String chapter, String question, Boolean hasImage, String image, ArrayList<Option> options) {
-		m_chapter = chapter;
+	public Question(String question, Boolean hasImage, String image, ArrayList<Option> options) {
 		m_text = question;
 		m_hasImage = hasImage;
 		
@@ -27,10 +24,6 @@ public class Question {
 
 	public boolean getHasImage() {
 		return m_hasImage;
-	}
-
-	public String getChapter() {
-		return m_chapter;
 	}
 
 	public String getText() {
