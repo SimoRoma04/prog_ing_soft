9fc55c184978ed689be097edb3c487336d8e0304
Federico Aguzzi <federico.aguzzi@icloud.com>
2022-01-30 14:48:00 +0100

    Refactored constructor, finished loadPage() implementation

--- code/model/GuiManager.java
+++ code/model/GuiManager.java
@@ -2,14 +2,11 @@ package model;
 
 import java.awt.BorderLayout;
 import java.awt.Component;
-import java.util.ArrayList;
-
 import javax.swing.JFrame;
-import javax.swing.JList;
-import javax.swing.JPanel;
 import javax.swing.SwingUtilities;
 
 import adapter.*;
+import view.AnswerQuestion;
 import view.Home;
 import view.ViewInterface;
 
@@ -20,14 +17,14 @@ public class GuiManager extends JFrame{
 	QuizAdapter m_quizAdapter;
 	//JFrame m_frame = new JFrame();
 	
-	public GuiManager()
+	public GuiManager(QuizManager quizManager)
 	{
 		super();
 		
 		m_guiManagerAdapter = new GuiManagerAdapter(this);
-		m_quizAdapter = new QuizAdapter();
+		m_quizAdapter = new QuizAdapter(quizManager);
 		
-		m_view = new Home(m_guiManagerAdapter, m_quizAdapter);
+		loadPage(PageTypes.P_HOME);
 		
 		super.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		super.setSize(800, 600);
@@ -45,8 +42,7 @@ public class GuiManager extends JFrame{
 	
 	public void loadPage(PageTypes type) {
 		super.setVisible(false);
-		super.removeAll();
-		m_view = null;
+		super.getContentPane().removeAll();
 		
 		switch(type){
 			case P_HOME:
@@ -62,12 +58,15 @@ public class GuiManager extends JFrame{
 				// m_view = new Statistics(m_guiManagerAdapter, m_quizAdapter);
 				break;
 			case P_QUIZ:
-				// m_view = new Quiz(m_guiManagerAdapter, m_quizAdapter);
+				m_view = new AnswerQuestion(m_guiManagerAdapter, m_quizAdapter);
 				break;
 		}
-		
-		super.setVisible(true);
+	
 		m_view.refresh();
+		SwingUtilities.updateComponentTreeUI(this);
+		super.revalidate();
+		super.repaint();
+		super.setVisible(true);
 		
 	}
 }
