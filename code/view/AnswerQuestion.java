43c0f42998531f2a123e5639683fc07adb2ea231
cripza <43786707+cripza@users.noreply.github.com>
2022-01-30 16:29:01 +0100

    AnswerQuestion: replaced QuizAdapter with PlayQuizAdapter

--- code/view/AnswerQuestion.java
+++ code/view/AnswerQuestion.java
@@ -14,7 +14,7 @@ import javax.swing.JPanel;
 import javax.swing.SwingUtilities;
 
 import adapter.GuiManagerAdapter;
-import adapter.QuizAdapter;
+import adapter.PlayQuizAdapter;
 import model.Image;
 import model.Option;
 import model.PageTypes;
@@ -22,12 +22,12 @@ import model.PageTypes;
 public class AnswerQuestion implements ViewInterface{
 
 	GuiManagerAdapter m_guiAdapter;
-	QuizAdapter m_quizAdapter;
+	PlayQuizAdapter m_playQuizAdapter;
 	ArrayList<JButton> m_buttons;
 	
-	public AnswerQuestion(GuiManagerAdapter guiAdapter, QuizAdapter quizAdapter) {
+	public AnswerQuestion(GuiManagerAdapter guiAdapter, PlayQuizAdapter quizAdapter) {
 		m_guiAdapter = guiAdapter;
-		m_quizAdapter = quizAdapter;
+		m_playQuizAdapter = quizAdapter;
 	}
 	
 	
@@ -62,7 +62,7 @@ public class AnswerQuestion implements ViewInterface{
 		
 		//-----
 		//inserimento del testo della domanda dal riferimento del controller
-		JLabel jLabel = new JLabel(m_quizAdapter.getQuestion().getText());
+		JLabel jLabel = new JLabel(m_playQuizAdapter.getQuestion().getText());
 		
 		//INSERIMENTO BOTTONE PER LA HOME
 		JPanel sup = new JPanel();
@@ -94,8 +94,8 @@ public class AnswerQuestion implements ViewInterface{
 		
 		
 		// Inserimento dell'immagine (se c'è)
-		if(m_quizAdapter.getQuestion().getHasImage()) {
-			Image img = m_quizAdapter.getQuestion().getImage();
+		if(m_playQuizAdapter.getQuestion().getHasImage()) {
+			Image img = m_playQuizAdapter.getQuestion().getImage();
 			ImageIcon pic = img.getImage();
 			label.setIcon(pic);
 		}
@@ -104,13 +104,13 @@ public class AnswerQuestion implements ViewInterface{
 		panel.get(4).add(label); 
 		
 		// Inserimento del testo della domanda dal riferimento del controller
-		panel.get(0).add(new JLabel(m_quizAdapter.getQuestion().getText()));
+		panel.get(0).add(new JLabel(m_playQuizAdapter.getQuestion().getText()));
 		
 		// Inizializzazione dell'ArrayList dei bottoni
 		m_buttons = new ArrayList<JButton>();
 		
 		// Creazione di un bottone per ogni domanda
-		for (Option o: m_quizAdapter.getQuestion().getOptions()) {
+		for (Option o: m_playQuizAdapter.getQuestion().getOptions()) {
 			JButton temp = new JButton(o.getText());
 			temp.addActionListener(e -> System.out.println("prova pulsante" + o.getText()));  //al posto della stringa bisogna inserire il metodo che viene invocato
 			m_buttons.add(temp);
